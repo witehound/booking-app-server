f959f877aebed88803601d52e76de73cd45c6cbe
witehound <netvid00@gmail.com>
2023-01-25 21:05:54 +0100

    creeated file structure for routes and controllers

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 import express from "express";
 import dotenv from "dotenv";
 import { createMongooseConnect } from "./lib/index.js";
+import {
+  AuthRouter,
+  UserRouter,
+  RoomRouter,
+  HotelRouter,
+} from "./routes/index.js";
 
 const app = express();
 dotenv.config();
-
 createMongooseConnect();
+
+//middlewarees
+app.use("/api/auth/", AuthRouter);
+app.use("/api/user/", UserRouter);
+app.use("/api/room/", RoomRouter);
+app.use("/api/hotel/", HotelRouter);
 
 app.listen(9898, () => {
   console.log("connected to back end.");
---
+++ routes/index.js
@@ -0,0 +1,6 @@
+import AuthRouter from "./auth.js";
+import UserRouter from "./user.js";
+import RoomRouter from "./room.js";
+import HotelRouter from "./hotel.js";
+
+export { AuthRouter, UserRouter, RoomRouter, HotelRouter };
