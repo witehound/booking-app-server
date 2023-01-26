600f811f048544eb0e2c22a6b29ae93e2c1ab4a4
witehound <netvid00@gmail.com>
2023-01-26 15:20:24 +0100

    completed login route and controller for auth

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 import { UserModel } from "../models/index.js";
 import { hashPassword } from "../lib/index.js";
+import { createError, compareHashPassword } from "../lib/index.js";
 
 const register = async (req, res, next) => {
   const { userName, email, password } = req.body;
@@ -17,15 +18,20 @@ const register = async (req, res, next) => {
 };
 
 const login = async (req, res, next) => {
-  const { userName, email, password } = req.body;
-  const newUser = new UserModel({
-    userName,
-    email,
-    password: hashPassword(password),
-  });
   try {
-    const savedUser = await newUser.save();
-    res.status(200).json({ message: `user has beeen created` });
+    const user = await UserModel.findOne({ userName: req.body.userName });
+    if (!user) {
+      next(createError(404, "User not found"));
+    }
+    const isPasswordValid = await compareHashPassword(
+      req.body.password,
+      user.password
+    );
+    if (!isPasswordValid) {
+      next(createError(400, "Wrong password or username"));
+    }
+    const { password, isAdmin, ...otherDetails } = user._doc;
+    res.status(200).json({ ...otherDetails });
   } catch (error) {
     next(error);
   }
