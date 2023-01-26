0ae0f59e86a44f5cefb822c2722bff6c2cddf507
witehound <netvid00@gmail.com>
2023-01-26 14:47:53 +0100

    compeleted user schema

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,15 @@
 import { UserModel } from "../models/index.js";
 
 const register = async (req, res, next) => {
-    
+  const { userName, email, password } = req.body;
+  const newUser = new UserModel({
+    userName,
+    email,
+    password,
+  });
   try {
+    const savedUser = await newUser.save();
+    res.satus(200).json({ message: `user has beeen created` });
   } catch (error) {
     next(error);
   }
--- models/user.js
+++ models/user.js
@@ -1,9 +1,29 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  userName: {
-    type: String,
+const userSchema = new mongoose.Schema(
+  {
+    userName: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
   },
-});
+  {
+    timestamps: true,
+  }
+);
 
 export default mongoose.model("user", userSchema);
